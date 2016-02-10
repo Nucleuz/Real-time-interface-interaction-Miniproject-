8deb7dd4dc8994d6b5f9e287cf86c027b10f5e0f
Nucleuz <h-hansen1990@hotmail.com>
2016-02-10 10:42:21 +0100

    Added obstacles that spawn randomly.

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject walls;
+    int score = 0;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("CreateObstacle", 1f, 1f);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void CreateObstacle()
+    {
+        Instantiate(walls);
+        score++;
+    }
+
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+        GUILayout.Label(" Score: " + score.ToString());
+
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(GameObject.FindWithTag("Walls"));
+    }
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour
+{
+
+    public Vector2 velocity = new Vector2(-4, 0);
+	public float range;
+    // Use this for initialization
+    void Start()
+    {
+		range = Random.Range(-5f,5f);
+        GetComponent<Rigidbody2D>().velocity = velocity;
+        transform.position = new Vector3(transform.position.x+10, transform.position.y + range, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (transform.position.x < -10) {
+			Destroy (this.gameObject);
+		}
+    }
+
+	void OnCollisionEnter2D (Collision2D other){
+		if (other.gameObject.CompareTag ("Player")) {
+			Destroy (this.gameObject);
+		}
+	}
+}
