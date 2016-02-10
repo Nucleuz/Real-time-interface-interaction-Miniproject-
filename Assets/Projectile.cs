b31983e33ca9cf9571b6aed227a9b460997372c4
Nucleuz <h-hansen1990@hotmail.com>
2016-02-10 14:50:57 +0100

    Added a projectile prefab and script

---
+++ Assets/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public float projectileSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position + new Vector3(projectileSpeed, 0, 0);
+
+        if (transform.position.x > 20)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
