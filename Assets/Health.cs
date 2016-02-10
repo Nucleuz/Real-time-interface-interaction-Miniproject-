acc75545c7ec429c6aed3e40f38dd7868ff593cf
Nucleuz <h-hansen1990@hotmail.com>
2016-02-10 14:52:05 +0100

    Added health text to display HealthPoints

---
+++ Assets/Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    Text txt;
+    public Player player;
+
+	// Use this for initialization
+	void Start () {
+        txt = GetComponent<Text>();
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        txt.text = "Health: " + player.healthPoints.ToString();
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,15 @@ public class Player : MonoBehaviour
     public Vector2 jumpForce = new Vector2(0, 200);
 	private Rigidbody2D rb2d;
 
+    public GameObject projectile;
+
+    public int healthPoints = 5;
 
     // Use this for initialization
     void Start()
     {
 		rb2d = GetComponent<Rigidbody2D> ();
+        
     }
 
     // Update is called once per frame
@@ -21,24 +25,34 @@ public class Player : MonoBehaviour
 		float h = Input.GetAxis ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
 
+        // Move the player by calculating with moveSpeed
 		rb2d.velocity = new Vector2 (h * moveSpeed, v * moveSpeed);
-		/*
-		if(h*rb2d.velocity.x < maxSpeed){
-			rb2d.AddForce(Vector2.right * h * moveForce);
-		}
 
-		if(Mathf.Abs(rb2d.velocity.x) > maxSpeed){
-			rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
-		}*/
+        // Shoot if key is pressed
+        if (Input.GetKeyDown("space"))
+        {
+            Shoot();
+        }
     }
 
     public void Jump()
     {
-		rb2d.velocity = Vector2.zero;
+        rb2d.velocity = Vector2.zero;
         rb2d.AddForce(jumpForce);
     }
 
+    public void Shoot()
+    {
+        GameObject g = (GameObject)Instantiate(projectile, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        
+        g.transform.parent = GameObject.Find("ProjectileContainer").transform;
+    }
 
+    public int DecreaseHealthPoints()
+    {
+        healthPoints--;
+        return healthPoints;
+    }
 
     //void OnCollisionEnter2D(Collision2D other)
     //{
