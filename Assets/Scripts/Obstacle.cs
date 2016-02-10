9ed6c2b0d55446fcc21b22b9609957ba7d0be792
Nucleuz <h-hansen1990@hotmail.com>
2016-02-10 14:53:47 +0100

    Decrease healthPoints of player if he hits obstacle

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class Obstacle : MonoBehaviour
 {
+    public Player player;
 
     public Vector2 velocity = new Vector2(-4, 0);
 	public float range;
     // Use this for initialization
     void Start()
     {
+        player = GameObject.Find("Player").GetComponent<Player>();
 		range = Random.Range(-5f,5f);
         GetComponent<Rigidbody2D>().velocity = velocity;
         transform.position = new Vector3(transform.position.x+10, transform.position.y + range, transform.position.z);
@@ -17,13 +19,14 @@ public class Obstacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (transform.position.x < -10) {
+		if (transform.position.x < -20) {
 			Destroy (this.gameObject);
 		}
     }
 
 	void OnCollisionEnter2D (Collision2D other){
 		if (other.gameObject.CompareTag ("Player")) {
+            player.DecreaseHealthPoints();
 			Destroy (this.gameObject);
 		}
 	}
